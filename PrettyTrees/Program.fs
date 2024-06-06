d849bf7cd085321fb742df431b32d05ee973c0d0
Dyberg <mathias.rerup-dyberg@hotmail.com>
2024-06-06 15:34:54 +0200

    stuuuuf

---
+++ PrettyTrees/Program.fs
@@ -0,0 +1,33 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+type Tree<'a> =
+    | Node of 'a * Tree<'a> list
+
+let moveTree (Node((label, x), subtrees) : Tree<string * float>) (x' : float) : Tree<string * float> =
+    Node((label, x + x'), subtrees)
+    
+type Extent = (float*float) list
+
+let moveExtent (e : Extent, x) = List.map(fun (p,q) -> (p+x,q+x)) e
+
+let rec merge : Extent * Extent -> Extent = function
+    | ([], qs) -> qs
+    | (ps, []) -> ps
+    | ((p, _)::ps, (_, q)::qs) -> (p, q) :: merge (ps, qs)
+
+
+let mergeList (es : Extent List) = List.fold (fun acc elem -> merge (acc, elem)) [] es
+
+
+
+
+
+
+
+
+
+
+
+
+
