2a6aa93e4a0e305d5e2ac8ca5bc382c258806515
Dyberg <mathias.rerup-dyberg@hotmail.com>
2024-06-10 11:22:52 +0200

    Update the CheckPositions as it was actually only checking the childs, this will make sure that all levels in the three gets tested

--- PrettyTrees/Program.fs
+++ PrettyTrees/Program.fs
@@ -97,16 +97,34 @@ printfn "%A" result
 
 open FsCheck
 
-let checkPositions (Node(_, subtrees)) = 
-    let positions = subtrees |> List.map (fun (Node((_, pos), _)) -> pos)
+let rec get_positions_level (Node(_, subtrees)) =
+    match subtrees with
+    | [] -> [[]]
+    | _ ->
+        let current_level = subtrees |> List.map (fun (Node((_, pos), _)) -> pos)
+        let next_levels = subtrees |> List.collect get_positions_level
+        current_level :: next_levels
+
+// Check that nodes are at least one unit apart on each level
+let checkPositions tree =
+    let positions_by_level = get_positions_level tree
     let rec check_positions = function
-        | [] | [_] -> true
-        | pos1::pos2::rest -> pos1 + 1.0 <= pos2 && check_positions (pos2::rest)
-    check_positions positions
+        | [] -> true
+        | level::rest ->
+            let rec check_level = function
+                | [] | [_] -> true
+                | pos1::pos2::rest -> pos1 + 1.0 <= pos2 && check_level (pos2::rest)
+            check_level level && check_positions rest
+    check_positions positions_by_level
 
 let ``Nodes should be at least one unit apart`` tree =
     let newTree = design tree
     checkPositions newTree
 
-
 let _ = Check.Quick ``Nodes should be at least one unit apart``
+
+
+
+
+
+
