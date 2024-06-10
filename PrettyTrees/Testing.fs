2e04c81a5517449b5ee06113c168e760dcb1fee9
Dyberg <mathias.rerup-dyberg@hotmail.com>
2024-06-10 14:03:35 +0200

    Implement propetybased testing for property 3 and 4. Property 3 is not working atm, it will require some questions to Michael

--- PrettyTrees/Testing.fs
+++ PrettyTrees/Testing.fs
@@ -23,8 +23,8 @@ let checkPositions tree =
     check_positions positions_by_level
 
 let ``Nodes should be at least one unit apart`` tree =
-    let newTree = design tree
-    checkPositions newTree
+    let designedTree = design tree
+    checkPositions designedTree
 
 //let _ = Check.Quick ``Nodes should be at least one unit apart``
 
@@ -42,7 +42,40 @@ let ``Parent should be centered above its children`` tree =
     test designedTree
 
 //let _ = Check.Quick ``Parent should be centered above its children``
+let rec reflect (Node(v, subtrees)) =
+    Node(v, List.map reflect (List.rev subtrees))
 
+let rec reflectPos (Node((v, x), subtrees)) =
+    Node((v, -x), List.map reflectPos subtrees)
+    
+
+let ``Symmetric tree should be the same tree`` tree =
+    let designedTree = design tree
+    let temp = reflectPos (design (reflect tree))
+    printfn "DesignTree: %A" designedTree
+    printfn "ReflectedTree: %A" temp
+    let pos1 = get_positions_level designedTree
+    let pos2 = get_positions_level temp
+    printfn "pos1: %A" pos1
+    printfn "pos2: %A" pos2
+    printfn "Equal %A" (pos1 = pos2)
+    pos1 = pos2
+
+let CompareTwoTrees (tree1 : Tree<'a * float>) (tree2 : Tree<'a>) : bool =
+    
+    let rec test (Node((label1, pos), children1)) (Node(_, children2)) =
+        match children1, children2 with
+        | _, []     -> true
+        | [], _     -> true
+        | _         -> tree1 = (design tree2) && List.forall2 test children1 children2
+    test tree1 tree2
+
+let ``Subtrees should be identical`` tree =
+    let designedTree = design tree
+    CompareTwoTrees designedTree tree
+    
 let runTests =
     Check.Quick ``Nodes should be at least one unit apart``
     Check.Quick ``Parent should be centered above its children``
+    //Check.Quick ``Symmetric tree should be the same tree``
+    Check.Quick ``Subtrees should be identical``
